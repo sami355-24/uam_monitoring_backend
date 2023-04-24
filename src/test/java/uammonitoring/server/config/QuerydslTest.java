ef8d0bbe2233b28cef04844104927ac3a14dd22f
sami355-24 <ddung355@gmail.com>
2023-04-25 00:00:57 +0900

    [Test]: Querydsl 테스트 코드 추가

--- src/test/java/uammonitoring/server/config/QuerydslTest.java
+++ src/test/java/uammonitoring/server/config/QuerydslTest.java
@@ -1,11 +1,13 @@
 package uammonitoring.server.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import uammonitoring.server.Entity.member;
+import uammonitoring.server.Repository.memberRepository;
 
 import java.util.List;
 
@@ -19,12 +21,26 @@ public class QuerydslTest {
     @Autowired
     JPAQueryFactory queryFactory;
 
+    @Autowired
+    memberRepository repository;
+
+    @Autowired
+    EntityManager em;
+
     @Test
     public void Querydsl() throws Exception{
         //given
         String name = "test";
+        uammonitoring.server.Entity.member createMember = uammonitoring.server.Entity.member.builder()
+                .name(name)
+                .password("test")
+                .build();
 
         //when
+        repository.save(createMember);
+
+        em.flush();
+        em.clear();
 
         List<member> members = queryFactory
                 .selectFrom(member)
