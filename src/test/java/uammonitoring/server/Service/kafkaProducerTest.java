ea3e59cd33fcbb4fe1da48b1a9bde0775e89c18d
sami355-24 <ddung355@gmail.com>
2023-04-28 15:28:58 +0900

    [Test]: 카프카 producer Test Code 추가

---
+++ src/test/java/uammonitoring/server/Service/kafkaProducerTest.java
@@ -0,0 +1,30 @@
+package uammonitoring.server.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uammonitoring.server.Entity.currentPosition;
+import uammonitoring.server.domain.Adsb.currentTime;
+import uammonitoring.server.domain.Adsb.flightData;
+import uammonitoring.server.domain.Adsb.flightIdentifier;
+
+@SpringBootTest
+class kafkaProducerTest {
+
+    @Autowired
+    kafkaProducer kp;
+
+    @Test
+    public void sendTest() throws Exception{
+        //given
+        flightIdentifier fi = new flightIdentifier("UAL123");
+        currentPosition cp = new currentPosition("400", "126.9525465", "37.5453577");
+        currentTime ct = new currentTime("111", "222", "333");
+        flightData dummyData = new flightData(fi, ct, cp);
+
+        //when
+        kp.sendMessage(dummyData);
+
+        //then
+    }
+}
