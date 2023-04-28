16fd8614051f2b898002ca9759f282affd0a17b0
sami355-24 <ddung355@gmail.com>
2023-04-28 15:28:06 +0900

    [Feat]: 카프카 producer, consumer 클래스 추가

---
+++ src/main/java/uammonitoring/server/Service/kafkaProducer.java
@@ -0,0 +1,19 @@
+package uammonitoring.server.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import uammonitoring.server.domain.Adsb.flightData;
+
+@Service
+@RequiredArgsConstructor
+public class kafkaProducer {
+
+    private static final String TOPIC = "test-topic";
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessage(flightData fd) {
+        kafkaTemplate.send(TOPIC, fd);
+    }
+}
