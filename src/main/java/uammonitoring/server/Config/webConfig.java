41a081b32f57640f50a0606f75d0f6d89209c2d3
sami355-24 <ddung355@gmail.com>
2023-05-05 18:11:58 +0900

    [Fix]: 컨버전 서비스 등록

--- src/main/java/uammonitoring/server/Config/webConfig.java
+++ src/main/java/uammonitoring/server/Config/webConfig.java
@@ -3,9 +3,7 @@ package uammonitoring.server.Config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import uammonitoring.server.Converter.fixmDTORouteToRouteConverter;
-import uammonitoring.server.Converter.fixmDTOToFixmConverter;
-import uammonitoring.server.Converter.fixmToFixmDTOConverter;
+import uammonitoring.server.Converter.*;
 
 @Configuration
 public class webConfig implements WebMvcConfigurer {
@@ -15,5 +13,7 @@ public class webConfig implements WebMvcConfigurer {
         registry.addConverter(new fixmDTOToFixmConverter());
         registry.addConverter(new fixmDTORouteToRouteConverter());
         registry.addConverter(new fixmToFixmDTOConverter());
+        registry.addConverter(new adsbDTOToAdsb());
+        registry.addConverter(new ListOfAdsbToListOfAdsbDTO());
     }
 }
