46f903f89c48df80b2cc96f9c215ef3bce3a7f33
sami355-24 <ddung355@gmail.com>
2023-05-12 16:15:48 +0900

    [Feat]: controller레이어에 completeFlight와 매핑되는 completeFlight 클래스 추가

--- src/main/java/uammonitoring/server/Controller/adsbController.java
+++ src/main/java/uammonitoring/server/Controller/adsbController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uammonitoring.server.DTO.adsbDTO;
 import uammonitoring.server.Service.adsbService;
@@ -31,5 +32,8 @@ public class adsbController {
         return allAdsb;
     }
 
-
+    @GetMapping("/completeFlight")
+    public void completeFlight(@RequestParam String uamIdentification) {
+        service.completeFlight(uamIdentification);
+    }
 }
