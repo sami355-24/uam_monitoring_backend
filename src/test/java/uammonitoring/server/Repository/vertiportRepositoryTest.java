c388c759c7c1eb1dd0ffb8869bdcc9a4154f93ff
sami355-24 <ddung355@gmail.com>
2023-05-12 11:04:22 +0900

    [Test]: Vertiport Repository vertiport 정보 추가하고 조회하는 기능 Test

---
+++ src/test/java/uammonitoring/server/Repository/vertiportRepositoryTest.java
@@ -0,0 +1,41 @@
+package uammonitoring.server.Repository;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Repository;
+import uammonitoring.server.Entity.vertiport;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class vertiportRepositoryTest {
+
+    @Autowired
+    private vertiportRepository repository;
+
+    @Test
+    @Transactional
+    public void insertVertiportTest() throws Exception{
+        //given
+        List<String> latitudes = List.of("1", "2", "3");
+        List<String> longitudes = List.of("1", "2", "3");
+
+        //when
+        for (int i = 0; i < 3; i++) {
+            repository.save(new vertiport(latitudes.get(i), longitudes.get(i)));
+        }
+
+        //then
+        List<vertiport> all = repository.findAll();
+        org.assertj.core.api.Assertions.assertThat(all).isNotNull();
+        for (vertiport vertiport : all) {
+            System.out.println(vertiport.toString());
+        }
+    }
+
+}
