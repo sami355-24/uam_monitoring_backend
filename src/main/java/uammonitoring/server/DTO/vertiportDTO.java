4f29b31c4a45482723f2e4bde15ef6921b4609d0
sami355-24 <ddung355@gmail.com>
2023-05-12 11:02:41 +0900

    [Feat]: vertiport 엔티티와 DTO 추가

---
+++ src/main/java/uammonitoring/server/DTO/vertiportDTO.java
@@ -0,0 +1,12 @@
+package uammonitoring.server.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class vertiportDTO {
+
+    private String latitude;
+    private String longitude;
+}
---
+++ src/main/java/uammonitoring/server/Entity/vertiport.java
@@ -0,0 +1,25 @@
+package uammonitoring.server.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@ToString
+public class vertiport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String latitude;
+    private String longitude;
+
+    public vertiport(String latitude, String longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
